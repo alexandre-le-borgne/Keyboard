82e6ff2fccdb0f3ae112d31cec3ff57850492978
Alexandre <alex83690@live.fr>
2016-05-05 18:44:05 +0200

    Labels

--- static/js/preset.js
+++ static/js/preset.js
@@ -19,18 +19,30 @@ var Constantes = function () {
 Constantes.BORDER_SIZE = 12;
 Constantes.DISTANCE_TO_PX = 42;
 
+var labelMap = [
+	  //0  1  2  3  4  5  6  7  8  9 10 11   // align flags
+		[ 0, 6, 2, 8, 9,11, 3, 5, 1, 4, 7,10], // 0 = no centering
+		[ 1, 7,-1,-1, 9,11, 4,-1,-1,-1,-1,10], // 1 = center x
+		[ 3,-1, 5,-1, 9,11,-1,-1, 4,-1,-1,10], // 2 = center y
+		[ 4,-1,-1,-1, 9,11,-1,-1,-1,-1,-1,10], // 3 = center x & y
+		[ 0, 6, 2, 8,10,-1, 3, 5, 1, 4, 7,-1], // 4 = center front (default)
+		[ 1, 7,-1,-1,10,-1, 4,-1,-1,-1,-1,-1], // 5 = center front & x
+		[ 3,-1, 5,-1,10,-1,-1,-1, 4,-1,-1,-1], // 6 = center front & y
+		[ 4,-1,-1,-1,10,-1,-1,-1,-1,-1,-1,-1] // 7 = center front & x & y
+];
+
 var convertPosition = {
     0: Positions.TOP_LEFT,
-    1: Positions.BOTTOM_LEFT,
+    1: Positions.TOP_CENTER,
     2: Positions.TOP_RIGHT,
-    3: Positions.BOTTOM_RIGHT,
+    3: Positions.MIDDLE_LEFT,
     4: Positions.FRONT,
-    5: Positions.FRONT,
-    6: Positions.MIDDLE_LEFT,
-    7: Positions.MIDDLE_RIGHT,
-    8: Positions.TOP_CENTER,
+    5: Positions.MIDDLE_RIGHT,
+    6: Positions.BOTTOM_LEFT,
+    7: Positions.BOTTOM_CENTER,
+    8: Positions.BOTTOM_RIGHT,
     9: Positions.MIDDLE_CENTER,
-    10: Positions.BOTTOM_CENTER,
+    10: Positions.FRONT,
     11: Positions.FRONT
 };
 
@@ -43,7 +55,7 @@ var defaultSettings = {
     r: 0,           // Angle de rotation
     rx: 0,          // Position du centre de rotation en x
     ry: 0,          // Position du centre de rotation en y
-    a: 0,           // Type de positionnement des labels
+    a: 4,           // Type de positionnement des labels
     c: "#fff",      // Couleur de la touche
     t: "#000"       // Couleur des labels
 };
@@ -55,7 +67,27 @@ Parser.getTextSize = function (size) {
     return 6 + 2 * size * (Constantes.DISTANCE_TO_PX / 42);
 };
 
-Parser.getLabels = function (element) {
+Parser.getTextAlign = function (position) {
+    var align = 'left';
+    var convertedPosition = convertPosition[position];
+    if(convertedPosition == Positions.TOP_CENTER || convertedPosition == Positions.BOTTOM_CENTER || convertedPosition == Positions.MIDDLE_CENTER)
+        align = 'center';
+    if(convertedPosition == Positions.TOP_RIGHT || convertedPosition == Positions.BOTTOM_RIGHT || convertedPosition == Positions.MIDDLE_RIGHT)
+        align = 'right';
+    return align;
+};
+
+Parser.getVerticalAlign = function (position) {
+    var align = 'top';
+    var convertedPosition = convertPosition[position];
+    if(convertedPosition == Positions.MIDDLE_LEFT || convertedPosition == Positions.MIDDLE_CENTER || convertedPosition == Positions.MIDDLE_RIGHT)
+        align = 'middle';
+    if(convertedPosition == Positions.BOTTOM_LEFT || convertedPosition == Positions.BOTTOM_CENTER || convertedPosition == Positions.BOTTOM_RIGHT)
+        align = 'bottom';
+    return align;
+};
+
+Parser.getLabels = function (labelPositionType, element) {
     var labels = [];
     if (element == "") {
         labels.push({position: convertPosition[0], data: ""});
@@ -63,17 +95,20 @@ Parser.getLabels = function (element) {
     }
     var sub = element.split("\n");
     var part;
-    for (var i in sub)
+    for (var i in sub) {
         part = sub[i];
-    if (typeof part == "string" && part != "")
-        labels.push({position: convertPosition[i], data: part});
+
+        if (typeof part == "string" && part != "")
+            labels.push({position: labelMap[labelPositionType][i], data: part});
+    }
     return labels;
 };
 
 Parser.ignoreFrontLabels = function (labels) {
     var result = [];
+
     for (var i in labels) {
-        if (labels[i].position != Positions.FRONT)
+        if (convertPosition[labels[i].position] != Positions.FRONT)
             result.push(labels[i])
     }
     return result;
@@ -96,7 +131,7 @@ Parser.parse = function (data) {
             var element = data[i][j];
 
             if (typeof element == "string") {
-                var labelsData = Parser.ignoreFrontLabels(Parser.getLabels(element));
+                var labelsData = Parser.ignoreFrontLabels(Parser.getLabels(currentSettings.a, element));
                 var labels = [];
                 for (var k in labelsData) {
                     labels.push(new Label(labelsData[k].data, labelsData[k].position, currentSettings.f, currentSettings.t));
@@ -238,14 +273,19 @@ var Key = function (labels, x, y, width, height, color, rotation_angle, rotation
 };
 
 var Label = function (value, position, size, color) {
-    this.element = $('<div></div>');
+    this.element = $('<div></div>').addClass('key-label').append(
+        $('<div></div>')
+    );
     this.value = value;
     this.position = position;
     this.color = color;
     this.size = size;
     this.draw = function () {
-        return this.element.html(value).css({
-            "font-size": Parser.getTextSize(this.size) + 'px'
+        this.element.find('div').html(this.value).css({
+            'font-size': Parser.getTextSize(this.size) + 'px',
+            'vertical-align': Parser.getVerticalAlign(this.position),
+            'text-align': Parser.getTextAlign(this.position)
         });
+        return this.element;
     };
 };
