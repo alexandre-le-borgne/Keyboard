bf47179379dc845ba4ced0296f0f1fea72c6a619
Alexandre <alex83690@live.fr>
2016-08-28 14:01:09 +0200

    V4-5
    Gestion des couleurs cachée
    Limitation de la taille des labels

--- keyboard/static/js/util.js
+++ keyboard/static/js/util.js
@@ -10,13 +10,19 @@
 //    }
 //};
 
-jQuery.extend( jQuery.fn, {
+jQuery.extend(jQuery.fn, {
     // Name of our method & one argument (the parent selector)
-    within: function( pSelector ) {
+    within: function (pSelector) {
         // Returns a subset of items using jQuery.filter
-        return this.filter(function(){
+        return this.filter(function () {
             // Return truthy/falsey based on presence in parent
-            return $(this).closest( pSelector ).length;
+            return $(this).closest(pSelector).length;
         });
     }
+});
+
+// Vérifier le respects des bornes des input type number lorsque le nombre est entré au clavier
+$('input[type="number"]').on("change", function () {
+    $(this).val() > $(this).attr("max") ? $(this).val($(this).attr("max")) : (
+        $(this).val() < $(this).attr("min") ? $(this).val($(this).attr("min")) : null);
 });
